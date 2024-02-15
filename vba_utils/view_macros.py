ea2b542319394c2c4cbb4ee03d6b8e1602cdc87b
Kairos-T <hestiatay@gmail.com>
2024-02-15 23:31:22 +0800

    feat: added colorama usage

--- vba_utils/view_macros.py
+++ vba_utils/view_macros.py
@@ -1,13 +1,16 @@
+from colorama import Fore
+
 
 def detect_macros(file):
     if file.detect_vba_macros():
+        print()
         print("The following macros were found:")
         for (filename, stream_path, vba_filename, vba_code) in file.extract_macros():
             print("-" * 80)
-            print("OLE stream:", stream_path)
-            print("VBA filename:", vba_filename)
+            print("OLE stream:", Fore.BLUE + stream_path)
+            print("VBA filename:", Fore.BLUE + vba_filename)
             print("-" * 80)
-            print(vba_code)
+            print(Fore.GREEN + vba_code)
     else:
         print("No VBA macros found! Exiting...")
         exit(0)
