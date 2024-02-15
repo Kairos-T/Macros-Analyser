09dd7a31f01512bf23f76a1b5d3306100804765e
Kairos-T <hestiatay@gmail.com>
2024-02-15 19:36:08 +0800

    feat: added macros info parsing

--- vba_utils/view_macros.py
+++ vba_utils/view_macros.py
@@ -1,7 +1,13 @@
 
 def detect_macros(file):
     if file.detect_vba_macros():
-        print("This document contains VBA macros.")
+        print("The following macros were found:")
+        for (filename, stream_path, vba_filename, vba_code) in file.extract_macros():
+            print("-" * 80)
+            print("OLE stream:", stream_path)
+            print("VBA filename:", vba_filename)
+            print("-" * 80)
+            print(vba_code)
     else:
         print("No VBA macros found! Exiting...")
         exit(0)
